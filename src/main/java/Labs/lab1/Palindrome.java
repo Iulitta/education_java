64a99e71acc2b02bf405299758c389ed55d6da49
Иулитта <iulittakatanaeva@yandex.ru>
2020-09-28 17:36:34 +0300

    Laba 1 completed

---
+++ src/main/java/Labs/lab1/Palindrome.java
@@ -0,0 +1,40 @@
+package Labs.lab1;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        if (isPalindrome(s)) {
+            System.out.println("Слово " + s + " является палиндромом.");
+        }
+        else {
+            System.out.println("Слово " + s + " не является палиндромом.");
+        }
+    }
+
+    /**
+     * Функция возвращает строку в реверсированном виде
+     * @param str строка
+     * @return реверсированная строка
+     */
+    public static String reverseString(String str) {
+        String temp = "";
+        int l = str.length() - 1;
+        while (l >= 0) {
+            temp = temp + str.charAt(l);
+            l--;
+        }
+        return temp;
+    }
+
+    /**
+     * Функция проверяет, является ли строка палиндромом
+     * @param s строка
+     * @return true, если строка - палиндром, или false, если нет
+     */
+    public static boolean isPalindrome(String s) {
+        return s.equals(reverseString(s));
+    }
+}
